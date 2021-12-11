7bc343a3597b41da7215f3d834ddbb1ed6db30ab
Prince Fefar <prince.fefar@dapperlabs.com>
2021-12-11 14:28:25 -0800

    values

---
+++ gobyexample/values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("harry" + " potter")
+
+	fmt.Println("5 * 5 = ", 5*5)
+	fmt.Println("3 / 2 = ", 3/2)
+	fmt.Println("3 / 2 = ", 3.0/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+}
