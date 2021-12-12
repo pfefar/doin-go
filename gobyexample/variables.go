5046748c06eccc670fc7c350930b7dac68b65164
Prince Fefar <prince.fefar@dapperlabs.com>
2021-12-11 20:15:10 -0800

    variables

---
+++ gobyexample/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e string
+	fmt.Println(e)
+
+	f := "orange"
+	fmt.Println(f)
+}
