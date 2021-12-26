d5c6e91a518b9ff898521a7a19689e8a10d84678
Prince Fefar <prince.fefar@dapperlabs.com>
2021-12-25 18:12:10 -0800

    arrays

---
+++ gobyexample/arrays.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [4]int
+	fmt.Println("arr", a)
+
+	a[3] = 100
+	fmt.Println("arr", a)
+	fmt.Println("length: ", len(a))
+
+	dna := [4]string{"A", "T", "C", "G"}
+	fmt.Println(dna)
+
+	var twoD [2][3]int
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+
+	fmt.Println(twoD)
+}
